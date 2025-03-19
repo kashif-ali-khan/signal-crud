18dd3edc4c3df9b2451be8bbaf16336eb5ef698f
Kashif Khan <kkhan@mutualink.net>
2025-03-19 13:13:30 +0530

    implements lazy loading of components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,18 @@
 import { Routes } from '@angular/router';
-import { ContactsListComponent } from './components/contacts-list/contacts-list.component';
-import { AddContactComponent } from './components/add-contact/add-contact.component';
-import { EditContactComponent } from './components/edit-contact/edit-contact.component';
+
+
+
 export const routes: Routes = [
     {
         path:'',
-        component: ContactsListComponent
+        loadComponent: () => import('./components/contacts-list/contacts-list.component').then(m => m.ContactsListComponent)
     },
     {
         path: 'add',
-        component: AddContactComponent
+        loadComponent: () => import('./components/add-contact/add-contact.component').then(m => m.AddContactComponent)
     },
     {
         path: 'edit/:id',
-        component: EditContactComponent
+        loadComponent: () => import('./components/edit-contact/edit-contact.component').then(m => m.EditContactComponent)
     }
 ];
